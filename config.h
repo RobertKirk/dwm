d31e85a83b81e19e53e9ce32f5930830b1ba37d4
Robert Kirk <robert.kirk.3.14@gmail.com>
2019-06-16 23:05:14 +0100

    adding usage of dmenu_run_favs command for favourites

--- config.h
+++ config.h
@@ -85,7 +85,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *termcmd[]  = { "st", NULL };
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenufavcmd[] = { "dmenu_run_favs", "-m", dmenumon, "-fn", dmenufont, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *lockcmd[]    = { "betterlockscreen", "-s", NULL };
 static const char *volmutecmd[] = { "volmute", NULL };
 static const char *volupcmd[]   = { "volup",   NULL };
@@ -96,7 +97,8 @@ static const char *songprevcmd[] = { "songprev", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenufavcmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_j,      focusstack,     {.i = +1 } },
